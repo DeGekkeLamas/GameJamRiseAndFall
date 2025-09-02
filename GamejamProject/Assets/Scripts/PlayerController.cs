de8df9b1088120aaa0b479c6d074fe5d41dceb8e
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-02 13:20:16 +0200

    movement done

--- GamejamProject/Assets/Scripts/PlayerController.cs
+++ GamejamProject/Assets/Scripts/PlayerController.cs
@@ -15,24 +15,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Walking
-        transform.position += walkSpeed * new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += walkSpeed * Time.deltaTime * new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 
         // Jumping
-        /// Collision testing
-        bool collisionLeft = Physics.Raycast(
-            this.transform.position - .49f * new Vector3(-this.transform.localScale.x, this.transform.localScale.y, 0),
-            Vector3.down, out RaycastHit hitinfoL, .1f);
-        bool collisionRight = Physics.Raycast(
-            this.transform.position - .49f * new Vector3(this.transform.localScale.x, this.transform.localScale.y, 0),
-            Vector3.down, out RaycastHit hitinfoR, .1f);
-        /// Display
-        Debug.DrawRay(this.transform.position - .5f * new Vector3(this.transform.localScale.x, this.transform.localScale.y, 0)
-            , Vector3.down * .1f, collisionRight ? Color.green : Color.red);
-        Debug.DrawRay(this.transform.position - .5f * new Vector3(-this.transform.localScale.x, this.transform.localScale.y, 0)
-            , Vector3.down * .1f, collisionLeft ? Color.green : Color.red);
-        /// Add velocity
-        if (Input.GetKeyDown(KeyCode.Space) && (collisionLeft || collisionRight))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+            /// Collision testing
+            bool collisionLeft = Physics.Raycast(
+                this.transform.position - .49f * new Vector3(-this.transform.localScale.x, this.transform.localScale.y, 0),
+                Vector3.down, .1f);
+            bool collisionRight = Physics.Raycast(
+                this.transform.position - .49f * new Vector3(this.transform.localScale.x, this.transform.localScale.y, 0),
+                Vector3.down, .1f);
+            /// Add velocity
+            if (collisionLeft || collisionRight)
             rigidbody.AddForce(Vector3.up * jumpForce);
         }
     }
