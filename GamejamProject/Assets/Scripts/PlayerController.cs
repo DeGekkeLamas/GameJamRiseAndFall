fd326135d1f4d2d2c50925d9f1d4d291b1b96c0b
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-02 13:11:04 +0200

    jumping works now

--- GamejamProject/Assets/Scripts/PlayerController.cs
+++ GamejamProject/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerController : MonoBehaviour
 {
     private new Rigidbody rigidbody;
-    public float walkSpeed;
-    public float jumpForce;
+    public float walkSpeed = 1;
+    public float jumpForce = 1;
 
     private void Awake()
     {
@@ -13,9 +14,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Walking
+        transform.position += walkSpeed * new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+        // Jumping
+        /// Collision testing
+        bool collisionLeft = Physics.Raycast(
+            this.transform.position - .49f * new Vector3(-this.transform.localScale.x, this.transform.localScale.y, 0),
+            Vector3.down, out RaycastHit hitinfoL, .1f);
+        bool collisionRight = Physics.Raycast(
+            this.transform.position - .49f * new Vector3(this.transform.localScale.x, this.transform.localScale.y, 0),
+            Vector3.down, out RaycastHit hitinfoR, .1f);
+        /// Display
+        Debug.DrawRay(this.transform.position - .5f * new Vector3(this.transform.localScale.x, this.transform.localScale.y, 0)
+            , Vector3.down * .1f, collisionRight ? Color.green : Color.red);
+        Debug.DrawRay(this.transform.position - .5f * new Vector3(-this.transform.localScale.x, this.transform.localScale.y, 0)
+            , Vector3.down * .1f, collisionLeft ? Color.green : Color.red);
+        /// Add velocity
+        if (Input.GetKeyDown(KeyCode.Space) && (collisionLeft || collisionRight))
         {
-            rigidbody.AddForce(Vector3.up);
+            rigidbody.AddForce(Vector3.up * jumpForce);
         }
     }
 }
