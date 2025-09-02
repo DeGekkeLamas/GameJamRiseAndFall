4c5de1df453767093593fbd50a568cf3e77762c6
arthur <ajcvanittersum@gmail.com>
2025-09-02 15:03:53 +0200

    testing max distance between trees
    changed tree spawning algorithm to test for max distance

--- GamejamProject/Assets/Scripts/SpawnTrees.cs
+++ GamejamProject/Assets/Scripts/SpawnTrees.cs
@@ -1,18 +1,20 @@
-using NUnit.Framework;
-using UnityEngine;
+
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 public class SpawnTrees : MonoBehaviour
 {
     public List<GameObject> prefabsOfTrees;
     public List<GameObject> spawnedTrees = new();
-
+    public List<Vector2> aditionalPositionsToCheckConnection;
     public int treeCount;
     public int minDistance;
+    public int maxDistance;
     public Transform treeContainer;
     public Vector2 spawnableAreaMin;
     public Vector2 spawnableAreaMax;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,14 +27,13 @@ public class SpawnTrees : MonoBehaviour
     {
         ClearTrees();
         List<Vector2> chosenPositions = new();
-        for (int i = 0; i < treeCount; i++)
-        {
+        do {
             Vector2 pos = PickRandomPosInArea(spawnableAreaMin, spawnableAreaMax, chosenPositions.ToArray());
             chosenPositions.Add(pos);
             int randomTreeType = Random.Range(0, prefabsOfTrees.Count);
             Vector3 position = new Vector3(pos.x, 0, pos.y);
             spawnedTrees.Add(Instantiate(prefabsOfTrees[randomTreeType], position, Quaternion.identity, treeContainer));
-        }
+        } while (TestSmallestDistance(chosenPositions.ToArray()));
     }
 
     private Vector2 PickRandomPosInArea(Vector2 min, Vector2 max, Vector2[] chosenPositions)
@@ -52,12 +53,14 @@ public class SpawnTrees : MonoBehaviour
                     goodPos = false;
                     break;
                 }
+
             }
         } while (!goodPos);
         return new Vector2(pos.x, pos.y);
     }
 
-    private void ClearTrees()
+    [ContextMenu("ClearTrees")]
+    public void ClearTrees()
     {
         foreach (var tree in spawnedTrees)
         {
@@ -65,4 +68,33 @@ public class SpawnTrees : MonoBehaviour
         }
         spawnedTrees.Clear();
     }
+
+    private bool TestSmallestDistance(Vector2[] chosenPositions)
+    {
+        if (chosenPositions.Length == 1) return true;
+
+        Vector2[] allPositionsToTest = chosenPositions.Concat(aditionalPositionsToCheckConnection).ToArray();
+        foreach (var pos1 in allPositionsToTest)
+        {
+            float closest = float.MaxValue;
+            foreach (var pos2 in chosenPositions)
+            {
+                if (pos1 == pos2)
+                {
+                    continue;
+                }
+                float dist = Vector2.Distance(pos1, pos2);
+                if (dist < closest) closest = dist;
+            }
+            if (closest > maxDistance)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    
+
 }
