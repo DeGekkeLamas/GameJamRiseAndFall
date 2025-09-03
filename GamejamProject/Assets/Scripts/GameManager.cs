e5fc3ba76913f19392aee250161f476e47a1f2f4
DeGekkeLamas <karsvdtoorn@gmail.com>
2025-09-03 13:34:49 +0200

    time travel animation

--- GamejamProject/Assets/Scripts/GameManager.cs
+++ GamejamProject/Assets/Scripts/GameManager.cs
@@ -5,17 +5,34 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    public RectTransform fadeEff;
+    public float fadeSpeed = 20;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)) BackwardTime();
-        if (Input.GetKeyDown(KeyCode.E)) ForwardTime();
+        if (Input.GetKeyDown(KeyCode.Q)) StartCoroutine(BackwardTimeAnimation());
+        if (Input.GetKeyDown(KeyCode.E)) StartCoroutine(ForwardTimeAnimation());
     }
 
     IEnumerator ForwardTimeAnimation()
     {
-
+        for (int i = 0; i < 1600/fadeSpeed; i++)
+        {
+            fadeEff.anchoredPosition = new Vector2(0f, (500 - -100 - i * fadeSpeed) / 2) * fadeEff.parent.localScale.x;
+            fadeEff.sizeDelta = new Vector2(0, -(500 + -100 - i * fadeSpeed)) * fadeEff.parent.localScale.x;
+            //fadeEff.sizeDelta -= new Vector2(0, fadeSpeed);
+            yield return new WaitForFixedUpdate();
+        }
 
         ForwardTime();
+        
+        for (int i = 0; i < 1600/fadeSpeed; i++)
+        {
+            fadeEff.anchoredPosition = new Vector2(0f, (-600 - -100 - i * -fadeSpeed) / 2) * fadeEff.parent.localScale.x;
+            fadeEff.sizeDelta = new Vector2(0, -(-600 + -100 - i * -fadeSpeed)) * fadeEff.parent.localScale.x;
+            //fadeEff.sizeDelta -= new Vector2(0, fadeSpeed);
+            yield return new WaitForFixedUpdate();
+        }
+
         yield return new();
     }
 
@@ -29,9 +46,24 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator BackwardTimeAnimation()
     {
-
+        for (int i = 0; i < 1600 / fadeSpeed; i++)
+        {
+            fadeEff.anchoredPosition = new Vector2(0f, (500 - -100 - i * fadeSpeed) / 2) * fadeEff.parent.localScale.x;
+            fadeEff.sizeDelta = new Vector2(0, -(500 + -100 - i * fadeSpeed)) * fadeEff.parent.localScale.x;
+            //fadeEff.sizeDelta -= new Vector2(0, fadeSpeed);
+            yield return new WaitForFixedUpdate();
+        }
 
         BackwardTime();
+
+        for (int i = 0; i < 1600 / fadeSpeed; i++)
+        {
+            fadeEff.anchoredPosition = new Vector2(0f, (-600 - -100 - i * -fadeSpeed) / 2) * fadeEff.parent.localScale.x;
+            fadeEff.sizeDelta = new Vector2(0, -(-600 + -100 - i * -fadeSpeed)) * fadeEff.parent.localScale.x;
+            //fadeEff.sizeDelta -= new Vector2(0, fadeSpeed);
+            yield return new WaitForFixedUpdate();
+        }
+
         yield return new();
     }
 
